48353ec7dd135f338bf61da2c6c399dfa93b1b65
iagoCaires <cairesiago07@gmail.com>
2020-06-06 12:28:02 -0300

    App

---
+++ server/src/database/migrations/02_create_point_items.ts
@@ -0,0 +1,16 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex){
+    //CRIAR A TABELA
+    return knex.schema.createTable('points_items', table => {
+        table.increments('id').primary();
+
+        table.integer('point_id').notNullable().references('id').inTable('points');
+        table.integer('item_id').notNullable().references('id').inTable('items');
+    });
+}
+
+export async function down(knex: Knex){
+    //VOLAR ATRAS (DELETAR A TABELA)
+    return knex.schema.dropTable('points_items');
+}
